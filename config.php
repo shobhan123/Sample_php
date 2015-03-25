a9d7eb5b464cd6a6cd727893b305b74e4d7d114e
Shobhan <shobhan@multipliersolutions.in>
2015-03-25 11:30:56 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+/*This file consists of data base connection for Product
+AUTHOR: aditya
+Date: 06-03-2014  
+*/
+
+//$db_host="healthcampaign.db.11827976.hostedresource.com";
+$db_host="182.50.141.241";
+$db_name = "suburban";
+$db_user ="suburban";
+$db_password ="Sumareddy$1";
+
+$smscredit_price=0.11;
+?>
